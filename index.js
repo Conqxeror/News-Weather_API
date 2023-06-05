04b32c4220cd41e5bdbd20433ac91a3a1382d00b
Wali Mohammad Kadri <kadriwalimohammad@gmail.com>
2023-06-05 12:38:26 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+
+
 
 const app = express();
 const PORT = 3000; // Change the port number if needed
-const API_KEY = '344cb3455ba1f4b98908ed7665d1494a'; // Replace with your OpenWeatherMap API key
+const API_KEY = "YOUR_API_KEY_HERE";
 
-app.use(express.static('client'));
+app.use(cors());
 
-app.get('/weather', (req, res) => {
-  const city = req.query.city;
-  
-  if (!city) {
-    return res.send({ error: 'Please provide a city name.' });
-  }
+app.set("view engine", "ejs");
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+app.use(express.static(path.join(__dirname, "public")));
 
-  axios.get(apiUrl)
-    .then(response => {
-      const weatherData = response.data;
-      const weatherDetails = {
-        city: weatherData.name,
-        description: weatherData.weather[0].description,
-        temperature: weatherData.main.temp
-      };
+app.get("/", (req, res) => {
+  res.render("News");
+});
 
-      res.send(weatherDetails);
+app.get("/weather", (req, res) => {
+  const city = req.query.city; // Get the city from the query parameters
+  const weatherData = {}; // Store weather data here
+
+  // Make a request to the weather API using the city name
+  axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+    .then((response) => {
+      // Extract the relevant data from the API response
+      weatherData.temperature = response.data.main.temp;
+      weatherData.weatherDescription = response.data.weather[0].description;
+      weatherData.location = `${response.data.name}, ${response.data.sys.country}`;
+
+      // Render the weather.ejs template and pass the weather data
+      res.render("Weather", { weatherData });
     })
-    .catch(error => {
-      res.send({ error: 'Unable to fetch weather data.' });
+    .catch((error) => {
+      console.error(error);
+      res.render("Weather", { weatherData: null }); // Render with null data if an error occurs
     });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
